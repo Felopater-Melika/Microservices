2a1e851070d155af134189d2d2a63bd98f561461
Felopater Melika <felopatermelika@gmail.com>
2023-08-14 13:52:23 -0500

    initial commit

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,63 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public static class PrepDb
+{
+    public static void PrepPopulation(IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        {
+            var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+            if (context == null)
+            {
+                Console.WriteLine("--> Context is null. Cannot seed data!");
+                return;
+            }
+
+            SeedData(context);
+        }
+    }
+
+    private static void SeedData(AppDbContext context)
+    {
+        if (context.Platforms == null)
+        {
+            Console.WriteLine("--> Platforms DbSet is null. Cannot seed data!");
+            return;
+        }
+
+        if (!context.Platforms.Any())
+        {
+            Console.WriteLine("--> Seeding Data...");
+
+            context.Platforms.AddRange(
+                new Platform()
+                {
+                    Name = "Dot Net",
+                    Publisher = "Microsoft",
+                    Cost = "Free"
+                },
+                new Platform()
+                {
+                    Name = "SQL Server",
+                    Publisher = "Microsoft",
+                    Cost = "Free"
+                },
+                new Platform()
+                {
+                    Name = "Kubernetes",
+                    Publisher = "Cloud Native Computing Foundation",
+                    Cost = "Free"
+                }
+            );
+
+            context.SaveChanges();
+        }
+        else
+        {
+            Console.WriteLine("--> We already have data!");
+        }
+    }
+}
