735bac2dd8e81878535701b813395095af6b04e7
Felopater Melika <felopatermelika@gmail.com>
2023-08-15 17:30:36 -0500

    null checks

--- CommandsService/Dtos/PlatformReadDto.cs
+++ CommandsService/Dtos/PlatformReadDto.cs
@@ -3,6 +3,6 @@ namespace CommandsService.Dtos
     public class PlatformreadDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
---
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using CommandsService.Dtos;
+using CommandsService.Models;
+
+namespace CommandsService.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Platform, PlatformreadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<PlatformPublishedDto, Platform>()
+                .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
